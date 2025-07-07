3138927f4678581d5a53b63df0eb14ee75a820b8
CrystalMuthui <crystalmuthui@gmail.com>
2025-07-07 20:21:18 +0300

    loin

--- app/src/main/java/com/example/studentconnect/activities/LoginActivity.kt
+++ app/src/main/java/com/example/studentconnect/activities/LoginActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studentconnect.R
 import com.google.firebase.auth.FirebaseAuth
+import com.example.studentconnect.activities.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
--- app/src/main/java/com/example/studentconnect/activities/MainActivity.kt
+++ app/src/main/java/com/example/studentconnect/activities/MainActivity.kt
@@ -1,2 +1,13 @@
 package com.example.studentconnect.activities
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.studentconnect.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
